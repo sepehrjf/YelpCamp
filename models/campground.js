cf38e004d6c993053b93ea026f972982fb92ad83
Sepehr <sep.farahani@gmail.com>
2021-03-22 19:57:06 -0700

    Adding The Reviews Model

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Review = require('./review');
 //reference to mongoose.Schema to make it shorter for rest of the code
 const Schema = mongoose.Schema;
 
@@ -8,7 +8,24 @@ const campgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+//this middleware is triggered by Campground.findByIdAndDelete in the app.js
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 });
 
 module.exports = mongoose.model('Campground', campgroundSchema);
