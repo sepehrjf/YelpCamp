144544702499477260b2f8274130f9437dd943be
Sepehr <sep.farahani@gmail.com>
2021-04-16 19:59:18 -0700

    fixing mobile bootstrap

--- app.js
+++ app.js
@@ -95,11 +95,11 @@ const scriptSrcUrls = [
 //This is the array that needs added to
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com",
     "https://api.mapbox.com/",
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/"
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
